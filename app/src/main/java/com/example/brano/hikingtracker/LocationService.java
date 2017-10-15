ef597d19cb203d3dd8b98ffdaaa429dcb9d243a8
bbello25 <bel0104@vsb.cz>
2017-10-15 16:55:33 +0200

    Added Location Service. Location Service used in TrackerFragment. [Not tested]

---
+++ app/src/main/java/com/example/brano/hikingtracker/LocationService.java
@@ -0,0 +1,96 @@
+package com.example.brano.hikingtracker;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+public class LocationService extends Service implements LocationListener, GpsStatus.Listener {
+    public static final String LOG_TAG = LocationService.class.getSimpleName();
+
+    private final LocationServiceBinder binder = new LocationServiceBinder();
+
+    public class LocationServiceBinder extends Binder {
+        public LocationService getService() {
+            return LocationService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public void startUpdatingLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(false);
+        criteria.setSpeedRequired(false);
+        //This flag enables LocationManager to exchange “data packet”
+        // with 3G/4G network base stations in order to get better location.
+        // This additional data packet exchange may increase the cost
+        // for the user’s monthly or temporary data plan.
+        criteria.setCostAllowed(false);
+        criteria.setBearingRequired(false);
+
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+        Integer gpsFreqInMillis = 1000;
+        Integer gpsFreqInDistance = 1;  // in meters
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (locationManager != null) {
+            locationManager.addGpsStatusListener(this);
+            locationManager.requestLocationUpdates(gpsFreqInMillis, gpsFreqInDistance, criteria, this, null);
+        }
+
+    }
+
+
+    @Override
+    public void onGpsStatusChanged(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(LOG_TAG, "(" + location.getLatitude() + "," + location.getLongitude() + ")");
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
