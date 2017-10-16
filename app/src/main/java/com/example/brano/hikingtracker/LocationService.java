244f5aecada679cda2d603335acebd7c680ca70e
bbello25 <bel0104@vsb.cz>
2017-10-16 18:32:07 +0200

    Added Location Service. Location Service used in TrackerFragment. [Not tested]

--- app/src/main/java/com/example/brano/hikingtracker/LocationService.java
+++ app/src/main/java/com/example/brano/hikingtracker/LocationService.java
@@ -52,13 +52,6 @@ public class LocationService extends Service implements LocationListener, GpsSta
         Integer gpsFreqInDistance = 1;  // in meters
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         if (locationManager != null) {
